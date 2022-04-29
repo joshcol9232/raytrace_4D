a474da27ccf4c918199054001d5e4b2c3d8f4c9b
Joshua Colclough <colcloughj2@cardiff.ac.uk>
2022-04-29 21:37:40 +0100

    Make a 3D one first maybe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use nalgebra::{Vector4};
+
+struct Ray {
+    n: Vector4<f32>, // Direction of travel
+    p: Point4<f32>,  // Point on the view plane
+}
+
+
+fn generate_viewport_rays(view_dir: Vector4<f32>, fov: f32) {
+    // Find viewport distance
+    let v = 
+}
+
 fn main() {
     println!("Hello, world!");
 }
